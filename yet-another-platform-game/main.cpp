bdfe37108f727692c96388fb2a395e502e4ec9e3
anpe9592 <anpe9592@users.noreply.github.com>
2016-08-07 12:54:52 +0200

    Batman

---
+++ yet-another-platform-game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  yet-another-platform-game
+//
+//  Created by Andreas Petersson on 2016-08-07.
+//  Copyright © 2016 Andreas Petersson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    //return 0;
+}
